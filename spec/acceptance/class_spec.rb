b684e1b226358dc8f336dfa957abb466368606d6
Peter Souter <p.souter@kainos.com>
2014-09-28 19:14:36 +0100

    Try adding shell commands...

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -13,5 +13,17 @@ describe 'wercker_example class' do
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
     end
+
+    it 'should allow shell commands' do
+      pp = <<-EOS
+      class { 'wercker_example': }
+      EOS
+
+      apply_manifest(pp, :expect_failures => false) do |r|
+        expect(r.stdout).to match(/Hello world/)
+      end
+
+      shell("echo 'Hello World' | grep 'Hello World'")
+    end
   end
 end
